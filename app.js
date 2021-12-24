279302889dad8b932df8baf3c7813b5914266544
bLanK <manish.unofficial993@gmail.com>
2021-12-24 17:18:14 +0530

    commit for heroku

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 const path = require('path')
 const pubDir = path.join(__dirname, 'public')
 console.log(pubDir);
@@ -8,6 +11,4 @@ app.use(express.static(pubDir))
 app.get('/', (req, res) => {
     res.send('<h1>Hello</>')
 })
-app.listen(port, (req, res) => {
-    console.log(`Listening to ${port}`);
-})
+app.listen(port);
